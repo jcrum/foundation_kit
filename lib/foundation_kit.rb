b194fa5b660b6efd1271aec18a1acfa9c0118dae
Jason Crum <jcrum@gci.com>
2018-04-02 22:28:09 -0800

    FIX require to avoid circular includes

--- lib/foundation_kit.rb
+++ lib/foundation_kit.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-Dir.glob("#{File.dirname(__FILE__)}/**/*.rb").sort.each { |path| require path }
+Dir.glob("#{File.dirname(__FILE__)}/foundation_kit/**/*.rb").sort.each { |path| require path }
 
 #
 # TODO
